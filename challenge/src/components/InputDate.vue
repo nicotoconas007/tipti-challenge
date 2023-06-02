3eddab14916ad6dad291a0d525604aa1ff4bce8b
Nicolas Toconas <ntoconas@lexim.solutions>
2023-06-02 20:33:42 -0300

    method to add a new input

--- challenge/src/components/InputDate.vue
+++ challenge/src/components/InputDate.vue
@@ -3,6 +3,9 @@
     <div v-for="(date, index) in dates" :key="index">
       <input type="date" v-model="date.date" />
     </div>
+    <button @click="addDate" :disabled="this.dates[0].date == ''">
+      Agregar Fecha
+    </button>
   </div>
 </template>
 
@@ -13,6 +16,17 @@ export default {
     return {
       dates: [{ date: "", isWeekend: false }],
     };
+  },
+  methods: {
+    addDate() {
+      const lastDate = this.dates[this.dates.length - 1];
+      const nextDate = new Date(lastDate.date);
+      nextDate.setDate(nextDate.getDate() + 1);
+      this.dates.push({
+        date: nextDate.toISOString().substring(0, 10),
+        isWeekend: false,
+      });
+    },
   },
 };
 </script>
